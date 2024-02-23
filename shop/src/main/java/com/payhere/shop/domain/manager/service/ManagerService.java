43eade85cf90dc86bcfb320d14bd17240d83d926
ksan <aquam41@gmail.com>
2024-02-23 18:07:18 +0900

    fix+fit : ManagerService 쿠키 관련 내용 코드 수정 및 CookieManager util 추가

--- shop/src/main/java/com/payhere/shop/domain/manager/service/ManagerService.java
+++ shop/src/main/java/com/payhere/shop/domain/manager/service/ManagerService.java
@@ -1,5 +1,6 @@
 package com.payhere.shop.domain.manager.service;
 
+import java.util.Objects;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Value;
@@ -11,6 +12,7 @@ import com.payhere.shop.domain.manager.dto.ManagerLoginDto;
 import com.payhere.shop.domain.manager.repository.ManagerRepository;
 import com.payhere.shop.domain.token.JwtTokenProvider;
 import com.payhere.shop.domain.token.dto.TokenDto;
+import com.payhere.shop.util.CookieManager;
 import com.payhere.shop.util.exception.NotAllowedManagerException;
 import com.payhere.shop.util.response.MetaData;
 import com.payhere.shop.util.service.EncryptionService;
@@ -19,7 +21,9 @@ import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class ManagerService {
@@ -45,43 +49,38 @@ public class ManagerService {
 
     public String login(ManagerLoginDto loginData, HttpServletResponse response) {
         String token = "";
-        Optional<Manager> findedManagerOp = managerRepository.findByPhone(loginData.getPhone());
-        if (!findedManagerOp.isPresent()) {
-            return token;
-        } else {
-            Manager findedManager = findedManagerOp.get();
+
+        try {
+            Manager findedManager = managerRepository.findByPhone(loginData.getPhone())
+                    .orElseThrow(
+                            () -> new Exception("manager not found"));
+
             if (!findedManager.getPassword().equals(encryptionService.encryptSha256(loginData.getPassword()))) {
                 return token;
             } else {
                 TokenDto tokenDt = jwtTokenProvider.createToken(findedManager.getPhone());
-
-                Cookie cookie = new Cookie(cookieName, tokenDt.getRefreshToken());
-                cookie.setPath("/");
-                response.addCookie(cookie);
-
+                CookieManager.addNewCookie(response, cookieName, tokenDt.getRefreshToken());
+                
                 token = tokenDt.getAccessToken();
             }
+        } catch (Exception e) {
+            log.error(e.getMessage());
         }
 
         return token;
     }
 
     public MetaData logout(HttpServletRequest request, HttpServletResponse response) {
-        Cookie[] cookies = request.getCookies();
+        Cookie cookie = CookieManager.getCookieByName(request, cookieName);
 
-        if (cookies != null) {
-            for (Cookie cookie : cookies) {
-                if (cookie.getName().equals(cookieName)) {
-                    String managerId = jwtTokenProvider.getManagerIdByToken(cookie.getValue());
-                    if (managerId.isBlank()) {
-                        return MetaData.NOT_ALLOWED_MANAGER;
-                    } else {
-                        jwtTokenProvider.saveRefreshToken(managerId, cookie.getValue());
-                        cookie.setMaxAge(0);
-                        response.addCookie(cookie);
-                        return MetaData.SUCCESS;
-                    }
-                }
+        if (Objects.nonNull(cookie)) {
+            String managerId = jwtTokenProvider.getManagerIdByToken(cookie.getValue());
+            if (managerId.isBlank()) {
+                return MetaData.NOT_ALLOWED_MANAGER;
+            } else {
+                jwtTokenProvider.saveRefreshToken(managerId, cookie.getValue());
+                CookieManager.expireCookie(response, cookie);
+                return MetaData.SUCCESS;
             }
         }
         return MetaData.NOT_ALLOWED_MANAGER;
